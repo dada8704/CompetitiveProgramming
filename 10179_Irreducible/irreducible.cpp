38b706b9fb928e7a0442007161e025743d53ecc1
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2021-01-12 15:15:56 +0800

    10179 done

---
+++ 10179_Irreducible/irreducible.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(void)
+{
+	int i, j, k;
+	int input;
+	vector<int> prime;
+	//prime.resize(3500);
+	//int prime[3500];
+	prime.push_back(2);
+
+	for(i=3;i*i<2000000000;i+=2)
+	{
+		int flag = 1;
+		for(const auto &it : prime)
+		{
+			if(i%it == 0)
+			{
+				flag = 0;
+				break;
+			}
+
+		}
+		if(flag) prime.push_back(i);
+	}
+	//for(const auto &it : prime)
+	//{
+	//	cout<<it<<" ";
+	//}
+	//cout<<endl<<prime.size()<<endl;
+
+	for(;;)
+	{
+		cin >> input;
+		int ans = input;
+		if(input == 0) break;
+		for(const auto &it : prime)
+		{
+			if(input % it == 0)
+			{
+				ans = ans / (it) * (it - 1);
+				for(;;)
+				{
+					input /= it;
+					if(input % it != 0) break;
+				}
+				
+			}
+		}
+		if(input == 1) cout << ans << endl;
+		else cout << ans / input * (input - 1) << endl;
+
+	}
+	
+	return 0;
+
+}
